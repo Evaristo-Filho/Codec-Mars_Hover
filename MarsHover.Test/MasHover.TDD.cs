7bbed6a7ff034ee5f3d4230ef1abf5ef762015e2
Evaristo Filho <evaristossfilho@hotmail.com>
2022-02-03 22:48:07 -0300

    Initial Step: Must Fail

--- MarsHover.Test/MasHover.TDD.cs
+++ MarsHover.Test/MasHover.TDD.cs
@@ -6,8 +6,10 @@ namespace MarsHover.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_Hover()
         {
+            Assert.Fail();
         }
+
     }
 }
