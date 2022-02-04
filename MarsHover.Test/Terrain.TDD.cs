27219b7a2c7c6aab4e14c482b5da733f54fae531
Evaristo Filho <evaristossfilho@hotmail.com>
2022-02-03 22:54:26 -0300

    Added TerrainTest: Must Fail

---
+++ MarsHover.Test/Terrain.TDD.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarsHover.Test
+{
+    [TestClass]
+    public class Terrain
+    {
+        [TestMethod]
+        public void Test_Hover()
+        {
+            Assert.Fail();
+        }
+
+    }
+}
